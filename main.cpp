3aeb5fcda1fdb226a7fca37bba19a2ac752ac23d
Vankxr <jgc3silva@gmail.com>
2018-03-25 04:06:15 +0100

    New features
    Started to add bootloader configs
    Commented the IVT patching code

--- main.cpp
+++ main.cpp
@@ -12,16 +12,56 @@
 #include <avr/wdt.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+#include <util/crc16.h>
 #include <stdint.h>
 #include <string.h>
+
+#define MAX_ROMS 5
+#define BOOT_MAGIC 0x5B
+
+#define BOOT_CONFIG_EE_ADDRESS ((void*)0xC00)
+
+// Structs & Enums
+enum boot_mode_t
+{
+	BOOT_MODE_NORMAL = 0,
+	BOOT_MODE_PIN,
+	BOOT_MODE_PIN_RESET,
+};
+enum boot_load_status_t
+{
+	BOOT_LOAD_STATUS_OFF = 0,
+	BOOT_LOAD_STATUS_ON,
+};
+
+struct boot_cfg_t
+{
+	uint8_t m_ubMagic;
+	uint8_t m_ubVersion;
+	boot_mode_t m_ubMode;
+	boot_load_status_t m_ubLoadStatus;
+	uint8_t m_ubCurrentROM;
+	uint8_t m_ubPinROM;
+	uint8_t m_ubLoadROM;
+	uint8_t m_ubROMCount;
+	uint32_t m_ulROMAddress[MAX_ROMS];
+	uint32_t m_ulLoadROMFlashAddress;
+	uint32_t m_ulLoadROMSize;
+	uint8_t m_ubCRC;
+};
+
+// Variables
+register uint8_t g_MCUSR asm("r2");
 
 // Forward declarations
 inline void resetMCU() __attribute__ ((__noreturn__));
 
-void flashProgramPage(uint32_t ulAddress, uint8_t *pubBuf, uint16_t uiSize = SPM_PAGESIZE);
-void flashReadVTable(uint32_t ulAddress, uint8_t *pubBuf);
+uint8_t validateConfig(boot_cfg_t* pConfig);
 
-void loadFirmware(uint32_t ulAddress);
+void flashProgramPage(uint32_t ulAddress, uint8_t *pubBuf, uint16_t uiSize = SPM_PAGESIZE);
+
+void loadROM(uint32_t ulAddress);
+
 
 void init()	__attribute__ ((naked)) __attribute__ ((section (".init3")));
 int main();
@@ -33,6 +73,31 @@ void resetMCU()
 	wdt_enable(WDTO_15MS);
 	
 	while(1);
+}
+
+uint8_t validateConfig(boot_cfg_t* pConfig)
+{	
+	if(pConfig->m_ubMagic != BOOT_MAGIC)
+		return 0;
+	
+	if(pConfig->m_ubCurrentROM >= MAX_ROMS)
+		return 0;
+	
+	if(pConfig->m_ubLoadStatus == BOOT_LOAD_STATUS_ON && pConfig->m_ubLoadROM >= MAX_ROMS)
+		return 0;
+	
+	if((pConfig->m_ubMode == BOOT_MODE_PIN || pConfig->m_ubMode == BOOT_MODE_PIN_RESET) && pConfig->m_ubPinROM >= MAX_ROMS)
+		return 0;
+	
+	uint16_t crc;
+	
+	for(uint16_t i = 0; i < sizeof(boot_cfg_t); i++)
+		crc = _crc16_update(crc, ((uint8_t*)pConfig)[i]);
+	
+	if(crc)
+		return 0;
+	
+	return 1;
 }
 
 void flashProgramPage(uint32_t ulAddress, uint8_t *pubBuf, uint16_t uiSize)
@@ -55,96 +120,101 @@ void flashProgramPage(uint32_t ulAddress, uint8_t *pubBuf, uint16_t uiSize)
 	boot_spm_busy_wait();
 	eeprom_busy_wait();
 }
-void flashReadVTable(uint32_t ulAddress, uint8_t *pubBuf)
-{
-	if(ulAddress + SPM_PAGESIZE > FLASHEND)
-		return;
-	
-	if(!pubBuf)
-		return;
-	
-	if(ulAddress > 0xFFFF)
-		memcpy_PF(pubBuf, ulAddress, _VECTORS_SIZE);
-	else
-		memcpy_P(pubBuf, (void*)ulAddress, _VECTORS_SIZE);
-}
 
-void loadFirmware(uint32_t ulAddress)
+void loadROM(uint32_t ulAddress)
 {
-	if(ulAddress < _VECTORS_SIZE)
+	if(ulAddress < _VECTORS_SIZE) // The (original) IVT space is required to be volatile (i.e. each firmware must have its own IVT copy, never flash to 0x00000)
+		return;
+		
+	if(ulAddress + _VECTORS_SIZE > FLASHEND)
 		return;
 	
 	// Live patch the interrupt vector table with one residing at the specified address
 	// The rest of the code can be run directly from that address
-	uint8_t vtBuf[_VECTORS_SIZE];
-	uint8_t pageIndex = 0;
+	uint8_t ivtBuf[_VECTORS_SIZE]; // Interrupt vector table
+	uint16_t pageIndex = 0; // Pages written (in case the VTable is bigger than one flash page)
 	
-	memset(vtBuf, 0, _VECTORS_SIZE);
+	memset(ivtBuf, 0, _VECTORS_SIZE); // Probably not needed
 	
-	flashReadVTable(ulAddress, vtBuf);
+	// Load the IVT from the desired address
+	if(ulAddress > 0xFFFF) // If the address is larger than 2 bytes we need a ELPM instead of a LPM
+		memcpy_PF(ivtBuf, ulAddress, _VECTORS_SIZE);
+	else
+		memcpy_P(ivtBuf, (void*)ulAddress, _VECTORS_SIZE);
 	
-	for(uint8_t i = 0; i < _VECTORS_SIZE; i += 4)
+	for(uint8_t i = 0; i < _VECTORS_SIZE; i += 4) // Each vector is 4 bytes in size (2 words)
 	{
-		uint32_t op = ((uint32_t)vtBuf[i] << 24) | ((uint32_t)vtBuf[i + 1] << 16) | ((uint32_t)vtBuf[i + 2] << 8) | vtBuf[i + 3];
+		uint32_t op = ((uint32_t)ivtBuf[i] << 24) | ((uint32_t)ivtBuf[i + 1] << 16) | ((uint32_t)ivtBuf[i + 2] << 8) | ivtBuf[i + 3];
 
-		op >>= 16;
-		op &= 0x00C0;
+		op >>= 16; // Lower byte would be masked anyways, so discard it
+		op &= 0x00C0; // Mask the offset and leave only the OP code
 
 		if(op == 0x00C0) // RJMP OP code
 		{
-			uint32_t destAddress = 0;
-			int16_t diffAddress = 0;
+			uint32_t destAddr = 0; // Destination (byte) address
+			int16_t diffAddr = 0; // Difference (word) address
 			
 			// Get the destination address from the RJMP instruction
-			diffAddress |= vtBuf[0];
-			diffAddress |= (vtBuf[i + 1] & 0x0F) << 8;
-			diffAddress |= (diffAddress & 0x0800) << 1; // Propagate the last bit
-			diffAddress |= (diffAddress & 0x0800) << 2;
-			diffAddress |= (diffAddress & 0x0800) << 3;
-			diffAddress |= (diffAddress & 0x0800) << 4;
+			diffAddr |= ivtBuf[0];
+			diffAddr |= (ivtBuf[i + 1] & 0x0F) << 8;
+			diffAddr |= (diffAddr & 0x0800) << 1; // Propagate the last bit for signed operations (2's complement)
+			diffAddr |= (diffAddr & 0x0800) << 2;
+			diffAddr |= (diffAddr & 0x0800) << 3;
+			diffAddr |= (diffAddr & 0x0800) << 4;
 
-			destAddress = ulAddress + i + 2 + diffAddress * 2; // "RJMP k" = "PC <- (k + 1)" (k is in words)
+			destAddr = ulAddress + i + 2 + diffAddr * 2; // "RJMP k" = "PC <- (k + 1)" (k is in words)
 			
-			// Convert the RJMP to a JMP
-			vtBuf[i] = (destAddress & 0x780000) >> 15;
-			vtBuf[i] |= 0x0C | ((destAddress & 0x040000) >> 18);
-			vtBuf[i + 1] = 0x94 | ((destAddress & 0x020000) >> 17);
-			vtBuf[i + 2] = (destAddress & 0x0001FE) >> 1;
-			vtBuf[i + 3] = (destAddress & 0x01FE00) >> 9;
+			// Convert the RJMP into a JMP to the absolute destination address (implicit byte-address to word-address conversion)
+			ivtBuf[i] = (destAddr & 0x780000) >> 15;
+			ivtBuf[i] |= 0x0C | ((destAddr & 0x040000) >> 18);
+			ivtBuf[i + 1] = 0x94 | ((destAddr & 0x020000) >> 17);
+			ivtBuf[i + 2] = (destAddr & 0x0001FE) >> 1;
+			ivtBuf[i + 3] = (destAddr & 0x01FE00) >> 9;
 		}
 		
-		if((i + pageIndex * SPM_PAGESIZE) >= SPM_PAGESIZE)
+		if((i + pageIndex * SPM_PAGESIZE) >= SPM_PAGESIZE) // If we have already modified one flash page, write it and increment the counter
 		{
-			flashProgramPage(pageIndex * SPM_PAGESIZE, vtBuf + pageIndex * SPM_PAGESIZE);
+			flashProgramPage(pageIndex * SPM_PAGESIZE, ivtBuf + pageIndex * SPM_PAGESIZE);
 			
 			pageIndex++;
 		}
 	}
 }
 
+
 // Main Program
 void init()
 {
 	cli(); // If any ISR is called during programming we're screwed
+	
+	g_MCUSR = MCUSR;
+	 
+	 MCUSR = 0x00;
+	 
+	 wdt_disable();
 }
 int main()
 {
-	register uint8_t _MCUSR = MCUSR;
+	boot_cfg_t bootConfig;
 	
-	MCUSR = 0x00;
+	memset(&bootConfig, 0, sizeof(boot_cfg_t));
 	
-	wdt_disable();
+	eeprom_busy_wait();
+	eeprom_read_block(&bootConfig, BOOT_CONFIG_EE_ADDRESS, sizeof(boot_cfg_t));
 	
-	if(_MCUSR & ((1 << EXTRF) | (1 << PORF))) // External & POR Reset
+	if(!validateConfig(&bootConfig))
+		return 1;
+		
+	if(g_MCUSR & ((1 << EXTRF) | (1 << PORF))) // External & POR Reset
 	{
 		
 	}
-	else if(_MCUSR & (1 << WDRF)) // Software (WDT) Reset
+	else if(g_MCUSR & (1 << WDRF)) // Software (WDT) Reset
 	{
 		
 	}
 	
-	loadFirmware(_VECTORS_SIZE);
+	loadROM(_VECTORS_SIZE);
 	
 	return 0;
 }
